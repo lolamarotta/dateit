8ae36c0696b4eb7ef0a41404ec24f7f74a6d789a
moraraimondi <moraraimondi@gmail.com>
2021-06-04 11:51:49 -0300

    todo me sale mal pero esto no :)

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react'; 
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export class App extends Component {
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
+export default App; 
 
 const styles = StyleSheet.create({
   container: {
@@ -19,3 +22,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
